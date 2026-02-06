d84ff23b42d65de89c75fb4fee389b39d7fa1d1f
piotr <gmail@gmail.com>
2026-02-06 14:34:20 +0100

    Dodanie testu do projektu

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,3 +1,5 @@
+using Cmd4DZad3;
+
 namespace TestProject1
 {
     [TestClass]
@@ -6,6 +8,14 @@ namespace TestProject1
         [TestMethod]
         public void TestMethod1()
         {
+            //Arrange
+            Samochod samochod = new Samochod("BMW", 2020, 10);
+            double wartoscOczekiwana = 500;
+            int iloscDni = 5;
+            //Act
+            double wynik = samochod.ObliczKosztWynajmu(iloscDni);
+            //Assert
+            Assert.AreEqual(wartoscOczekiwana, wynik, 0.0);
         }
     }
 }
